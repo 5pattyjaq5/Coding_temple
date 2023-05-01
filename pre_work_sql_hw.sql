e0bb689208102ee4df63641caa978ac4ab2dfb7e
5pattyjaq5 <131815687+5pattyjaq5@users.noreply.github.com>
2023-05-01 17:05:05 -0500

    Add files via upload

---
+++ pre_work_sql_hw.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS	classes(week INT, homework INT, classwork INT, projects INT, test INT, final_project INT, final_exams INT);
+
+INSERT INTO classes Values (1, 86, 95, 92, 95, 91, 92), (2, 88, 91, 93, 91, 89, 87), (3, 89, 93, 90, 94, 94, 86), (4, 87, 96, 85, 84, 86, 85), (5, 90, 91, 83, 86, 88, 85), (6, 85, 90, 90, 85, 90, 88), (7, 88, 92, 93, 87, 88, 84), (8, 90, 92, 91, 92, 92, 96) , (9, 86, 88, 84, 81, 82, 81), (10, 98, 89, 88, 92, 94, 95);
+
+Select * From classes
